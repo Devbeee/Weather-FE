f3db2a3f38a16557ce25066201a553e3ee289668
Devbeee <tngb1002@gmail.com>
2024-08-08 21:52:16 +0700

    add remove accents

---
+++ src/utils/common/removeAccents.js
@@ -0,0 +1,18 @@
+function removeAccents(str) {
+  const newStr = str
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/đ/g, "d")
+    .replace(/Đ/g, "D")
+    .replace(/\s+/g, " ")
+    .trim();
+  return newStr
+    .toLowerCase()
+    .split(" ")
+    .map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(" ");
+}
+
+export default removeAccents;
